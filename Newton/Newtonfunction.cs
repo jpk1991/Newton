591efdfa1fa1e8e0dc98c18cb92d5b4bedfd0ff9
Jan Philip Kozina <j.kozina@gmx.de>
2017-09-22 10:10:21 +0200

    constructor and getter/setter fixed

---
+++ Newton/Newtonfunction.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newton
+{
+    /// <summary>
+    /// Calculation of roots using the newtonmethod
+    /// </summary>
+    class Newtonfunction
+    {
+        private int orderOfRoot;
+
+        public int OrderOfRoot
+        {
+            get
+            {
+                return orderOfRoot;
+            }
+            set
+            {
+                orderOfRoot = value;
+            }
+        }
+
+        private float radicand;
+
+        public float Radicand
+        {
+            get
+            {
+                return radicand;
+            }
+            set
+            {
+                radicand = value;
+            }
+        }
+
+        private int precisionOfCalculation;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int PrecisionOfCalculation
+        {
+            get
+            {
+                return precisionOfCalculation;
+            }
+            set
+            {
+                precisionOfCalculation = value;
+            }
+        }
+
+        /// <summary>
+        /// Representation of the root in the newtonfunction and decimalprecision of the calculation
+        /// </summary>
+        /// <param name="orderOfRoot"> Order of the root</param>
+        /// <param name="radicand">Radicand of the root</param>
+        /// <param name="precisionOfCalculation">Precision of the calculation</param>
+        public Newtonfunction(int orderOfRoot, float radicand, int precisionOfCalculation)
+        {
+            this.orderOfRoot = orderOfRoot;
+            this.radicand = radicand;
+            this.precisionOfCalculation = precisionOfCalculation;
+        }
+
+        /// <summary>
+        /// Representation of the root in the newtonfunction and the decimalprecision of the calculation set to 2
+        /// </summary>
+        /// <param name="OrderOfRoot">Order of the root</param>
+        /// <param name="Radicand">Radicand of the root</param>
+        public Newtonfunction(int orderOfRoot, float radicand) : this(orderOfRoot, radicand, 2)
+        {
+        }
+
+        /// <summary>
+        /// Shows the current data of the root
+        /// </summary>
+        /// <returns>Stringrepresentation of the root</returns>
+        public override string ToString()
+        {
+            return $"{orderOfRoot} root of {radicand} with decimalprecision of {precisionOfCalculation}";
+        }
+    }
+}
---
+++ Newton/TestClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newton
+{
+    /// <summary>
+    /// Testclass with inputs
+    /// </summary>
+    class TestClass
+    {
+        public static void Main()
+        {
+            Newtonfunction function1 = new Newtonfunction(3, 27, 4);
+            Newtonfunction function2 = new Newtonfunction(3, 27);
+
+            Console.WriteLine(function1.ToString());
+            Console.WriteLine(function2.ToString());
+
+            function1.OrderOfRoot = 4;
+            Console.WriteLine(function1.ToString());
+            function2.PrecisionOfCalculation = 20;
+            Console.WriteLine(function2.ToString());
+
+            Console.ReadKey();
+        }
+
+    }
+}
